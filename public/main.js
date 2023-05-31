19aad1243d5972f180a1108379310d725bd9ccc1
jorovernier <jorovernier@gmail.com>
2023-05-31 11:58:22 -0700

    made a few small changes

--- public/main.js
+++ public/main.js
@@ -1,5 +1,5 @@
 const btn = document.querySelector('button')
 
-const clickHandler = () => alert('The cat name is: Rascal')
+const clickHandler = () => alert('The cat name is: Macy')
 
 btn.addEventListener('click', clickHandler)
